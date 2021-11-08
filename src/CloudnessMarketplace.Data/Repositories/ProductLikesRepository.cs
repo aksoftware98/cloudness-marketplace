a4dd0307cf8068df9df7b24015bcd19999ef58f8
Ahmad <aksoftware98@hotmail.com>
2021-11-08 10:43:03 +0200

    Improve the likes counting in the application

--- src/CloudnessMarketplace.Data/Repositories/ProductLikesRepository.cs
+++ src/CloudnessMarketplace.Data/Repositories/ProductLikesRepository.cs
@@ -13,16 +13,23 @@ namespace CloudnessMarketplace.Data.Repositories
         private readonly Database _db;
         private const string CONTAINER_NAME = "ProductLikes";
         private readonly Container _container;
-
-        public ProductLikesRepository(DbOptions options)
+        private readonly IProductsRepository _productsRepo;
+        public ProductLikesRepository(DbOptions options,
+                                      IProductsRepository productsRepo)
         {
             var cosmosClient = new CosmosClient(options.ConnectionString);
             _db = cosmosClient.GetDatabase(options.DatabaseName);
             _container = _db.GetContainer(CONTAINER_NAME);
+            _productsRepo = productsRepo;
         }
 
         public async Task LikeProductAsync(string productId, string userId)
         {
+            // Get the item by id 
+            var product = await _productsRepo.GetByIdAsync(productId, false);
+            if (product == null)
+                return; 
+
             await _container.CreateItemAsync<ProductLike>(new ProductLike
             {
                 Id = Guid.NewGuid().ToString(),
@@ -30,15 +37,28 @@ namespace CloudnessMarketplace.Data.Repositories
                 ProductId = productId,
                 UserId = userId,
             });
+
+            product.Likes++;
+            await _productsRepo.UpdateAsync(product);
         }
 
         public async Task RemoveLikeAsync(string productId, string userId)
         {
+            // Get the product
+            var product = await _productsRepo.GetByIdAsync(productId, false);
+            if (product == null)
+                return; 
+
+            product.Likes--;
+            
             var query = $"SELECT * FROM c WHERE c.productId = '{productId}' AND c.userId = '{userId}'";
             var iterator = _container.GetItemQueryIterator<ProductLike>(query);
             var result = await iterator.ReadNextAsync();
             if (result.Resource.Any())
+            {
                 await _container.DeleteItemAsync<ProductLike>(result.Resource.First().Id, new PartitionKey(userId));
+                await _productsRepo.UpdateAsync(product);
+            }
 
         }
     }
